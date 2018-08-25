817e6a1ef015e66f0da78cf7c5ba4b80d94154cb
Blomart Cedric <cedric@Cedrics-Mini-2.lan>
2018-08-25 23:05:33 +0200

    add logging

--- auth.go
+++ auth.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/golang/glog"
 )
 
 // HandleAuth Authenticates and resturns a token.
 func HandleAuth(w http.ResponseWriter, r *http.Request) {
+	glog.Infof("Call to authentication endpoint")
 	fmt.Fprintf(w, "Test page.")
 }
