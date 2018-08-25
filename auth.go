8229928675198c4d586549ec56680a05e5f300e4
Blomart Cedric <cedric@Cedrics-Mini-2.lan>
2018-08-26 00:36:51 +0200

    check auth request

--- auth.go
+++ auth.go
@@ -3,12 +3,107 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/golang/glog"
 )
 
+// AuthRequest respresent an authentication request
+type AuthRequest struct {
+	UserName string
+	Password string
+	ClientID string
+	Service  string
+	Scopes   []Scope
+}
+
+// Scope defined the required resources and actions
+type Scope struct {
+	Type    string
+	ID      string
+	Actions []string
+}
+
+// PasswordString hides passwords on output
+type PasswordString string
+
+func (s PasswordString) String() string {
+	if len(s) == 0 {
+		return ""
+	}
+	return "********"
+}
+
+// GetAuthRequest gets an authentication request from the http request
+func GetAuthRequest(r *http.Request) *AuthRequest {
+	ar := AuthRequest{}
+	if username, password, ok := r.BasicAuth(); ok {
+		ar.UserName = username
+		ar.Password = password
+	} else {
+		glog.Errorf("Authentication request didn't have basic authentication")
+		return nil
+	}
+	service := r.FormValue("service")
+	if len(service) == 0 {
+		glog.Errorf("No service provided")
+		return nil
+	}
+	ar.Service = service
+	clientid := r.FormValue("client_id")
+	if len(clientid) == 0 {
+		glog.Error("No ClientId provided")
+		return nil
+	}
+	scopeString := r.FormValue("scope")
+	if len(scopeString) == 0 {
+		glog.Errorf("No scopes provided")
+		return nil
+	}
+	scopes := strings.Split(scopeString, " ")
+	ar.Scopes = []Scope{}
+	for _, v := range scopes {
+		scope := GetScope(v)
+		if scope != nil {
+			ar.Scopes = append(ar.Scopes, *scope)
+		} else {
+			glog.Errorf("Could not parse scope %s", v)
+		}
+	}
+	return &ar
+}
+
+func (ar *AuthRequest) String() string {
+	return fmt.Sprintf("%s:%s client_id=%s service=%s scopes=%s", ar.UserName, ar.Password, ar.ClientID, ar.Service, ar.Scopes)
+}
+
+// GetScope ngets the scope from a string
+func GetScope(s string) *Scope {
+	scope := Scope{}
+	parts := strings.Split(s, ":")
+	switch len(parts) {
+	case 3:
+		scope.Type = parts[0]
+		scope.ID = parts[1]
+		scope.Actions = strings.Split(parts[2], ",")
+	case 4:
+		scope.Type = parts[0]
+		scope.ID = fmt.Sprintf("%s:%s", parts[1], parts[2])
+		scope.Actions = strings.Split(parts[3], ",")
+	default:
+		return nil
+	}
+	return &scope
+}
+
 // HandleAuth Authenticates and resturns a token.
 func HandleAuth(w http.ResponseWriter, r *http.Request) {
 	glog.Infof("Call to authentication endpoint")
+	// check parameters
+	ar := GetAuthRequest(r)
+	if ar == nil {
+		http.Error(w, "Authentication failed", http.StatusInternalServerError)
+	}
+	glog.Infof("Authentication Request: %s", ar.String())
 	fmt.Fprintf(w, "Test page.")
 }
