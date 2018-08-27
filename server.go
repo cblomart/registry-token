77ef8360be88c1d6566a21409bff3334af8dbff6
BLOMART CEDRIC J.F <cedric.blomart@minfin.fed.be>
2018-08-27 17:13:19 +0200

    change json to access token

--- server.go
+++ server.go
@@ -45,7 +45,7 @@ func (ss Scopes) String() string {
 // TokenResponse represents the response structure
 type TokenResponse struct {
 	Token       string `json:"token"`
-	AccessToken string `json:"token_access,omitempty"`
+	AccessToken string `json:"access_token,omitempty"`
 	ExpiresIn   int    `json:"expires_in,omitempty"`
 	IssuedAt    string `json:"issued_at,omitempty"`
 }
