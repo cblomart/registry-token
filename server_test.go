978cf9d0390c4314bb539912f9ea88ab1e450e99
BLOMART CEDRIC J.F <cedric.blomart@minfin.fed.be>
2018-09-01 00:26:13 +0200

    test get request

--- server_test.go
+++ server_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"net/http"
+	"net/url"
 	"reflect"
 	"testing"
 )
@@ -178,10 +179,100 @@ func TestGetAuthRequest(t *testing.T) {
 			name: "full request",
 			args: args{
 				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
 					Header: http.Header{
 						"Authorization": []string{
 							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
 						},
+					},
+					Form: url.Values{
+						"service":   []string{"registry.docker.io"},
+						"client_id": []string{"Docker"},
+						"scope":     []string{"repository:cblomart/foo:latest:pull,push"},
+					},
+				},
+			},
+			want: &AuthnRequest{
+				RemoteAddr: "127.0.0.1:54657",
+				UserName:   "username",
+				Password:   PasswordString("password"),
+				ClientID:   "Docker",
+				Service:    "registry.docker.io",
+				Scopes: Scopes{
+					Scope{
+						Type:    "repository",
+						Name:    "cblomart/foo:latest",
+						Actions: []string{"pull", "push"},
+					},
+				},
+			},
+		},
+		{
+			name: "request without scope",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"service": []string{"registry.docker.io"},
+					},
+				},
+			},
+			want: &AuthnRequest{
+				RemoteAddr: "127.0.0.1:54657",
+				UserName:   "username",
+				Password:   PasswordString("password"),
+				Service:    "registry.docker.io",
+				Scopes:     Scopes{},
+			},
+		},
+		{
+			name: "request without client id",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"service": []string{"registry.docker.io"},
+						"scope":   []string{"repository:cblomart/foo:latest:pull,push"},
+					},
+				},
+			},
+			want: &AuthnRequest{
+				RemoteAddr: "127.0.0.1:54657",
+				UserName:   "username",
+				Password:   PasswordString("password"),
+				Service:    "registry.docker.io",
+				Scopes: Scopes{
+					Scope{
+						Type:    "repository",
+						Name:    "cblomart/foo:latest",
+						Actions: []string{"pull", "push"},
+					},
+				},
+			},
+		},
+		{
+			name: "request without service",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"client_id": []string{"Docker"},
+						"scope":     []string{"repository:cblomart/foo:latest:pull,push"},
 					},
 				},
 			},
