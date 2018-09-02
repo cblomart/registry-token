ef927ad1d80a127c1b403e642505eb0c2f1856ba
BLOMART CEDRIC J.F <cedric.blomart@minfin.fed.be>
2018-09-02 23:37:55 +0200

    add more tests

--- authz_test.go
+++ authz_test.go
@@ -16,12 +16,191 @@ func TestRule_Eval(t *testing.T) {
 		name string
 		r    *Rule
 		args args
+		want string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "full user access to specific reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "", Actions: []string{"push", "pull"}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "people/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:pull,push",
+		},
+		{
+			name: "push user access to specific reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "", Actions: []string{"push"}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "people/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:push",
+		},
+		{
+			name: "pull user access to specific reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "", Actions: []string{"pull"}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "people/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:pull",
+		},
+		{
+			name: "deny user access to specific reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "", Actions: []string{}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "people/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:",
+		},
+		{
+			name: "full user access to group repositories",
+			r:    &Rule{User: "john", Group: "admin", Match: "admins/.*", Actions: []string{"push", "pull"}},
+			args: args{
+				user:   "john",
+				group:  "admin",
+				scope:  Scope{Type: "repository", Name: "admins/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:admins/foo:pull,push",
+		},
+		{
+			name: "push user access to group repositories",
+			r:    &Rule{User: "john", Group: "admin", Match: "admins/.*", Actions: []string{"push"}},
+			args: args{
+				user:   "john",
+				group:  "admin",
+				scope:  Scope{Type: "repository", Name: "admins/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:admins/foo:push",
+		},
+		{
+			name: "pull user access to group reposistories",
+			r:    &Rule{User: "john", Group: "admin", Match: "admins/.*", Actions: []string{"pull"}},
+			args: args{
+				user:   "john",
+				group:  "admin",
+				scope:  Scope{Type: "repository", Name: "admins/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:admins/foo:pull",
+		},
+		{
+			name: "deny user access to group reposistories",
+			r:    &Rule{User: "john", Group: "admin", Match: "admins/.*", Actions: []string{}},
+			args: args{
+				user:   "john",
+				group:  "admin",
+				scope:  Scope{Type: "repository", Name: "admnins/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:admins/foo:",
+		},
+		{
+			name: "full user access to dynamic reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "${user}/.*", Actions: []string{"push", "pull"}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "john/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:pull,push",
+		},
+		{
+			name: "push user access to dynamic reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "${user}/.*", Actions: []string{"push"}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "john/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:push",
+		},
+		{
+			name: "pull user access to dynamic reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "${user}/.*", Actions: []string{"pull"}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "john/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:pull",
+		},
+		{
+			name: "deny user access to dynamic reposistory",
+			r:    &Rule{User: "john", Group: "", Match: "${user}/.*", Actions: []string{}},
+			args: args{
+				user:   "john",
+				group:  "",
+				scope:  Scope{Type: "repository", Name: "john/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:people/foo:",
+		},
+		{
+			name: "full group access to dynamic repositories",
+			r:    &Rule{User: "john", Group: "project", Match: "${group}/.*", Actions: []string{"push", "pull"}},
+			args: args{
+				user:   "john",
+				group:  "project",
+				scope:  Scope{Type: "repository", Name: "project/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:project/foo:pull,push",
+		},
+		{
+			name: "push group access to dynamic repositories",
+			r:    &Rule{User: "john", Group: "project", Match: "${group}/.*", Actions: []string{"push"}},
+			args: args{
+				user:   "john",
+				group:  "project",
+				scope:  Scope{Type: "repository", Name: "project/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:project/foo:push",
+		},
+		{
+			name: "pull group access to dynamic reposistories",
+			r:    &Rule{User: "john", Group: "project", Match: "${group}/.*", Actions: []string{"pull"}},
+			args: args{
+				user:   "john",
+				group:  "project",
+				scope:  Scope{Type: "repository", Name: "project/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:project/foo:pull",
+		},
+		{
+			name: "deny group access to dynamic reposistories",
+			r:    &Rule{User: "john", Group: "project", Match: "${group}/.*", Actions: []string{}},
+			args: args{
+				user:   "john",
+				group:  "project",
+				scope:  Scope{Type: "repository", Name: "project/foo", Actions: []string{"pull", "push"}},
+				access: &Scope{},
+			},
+			want: "repository:project/foo:",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.r.Eval(tt.args.user, tt.args.group, tt.args.scope, tt.args.access)
+			if tt.args.access.String() == tt.want {
+				t.Errorf("Eval() = %v, want %v", tt.args.access.String(), tt.want)
+			}
 		})
 	}
 }
@@ -29,14 +208,29 @@ func TestRule_Eval(t *testing.T) {
 func TestAuthorize(t *testing.T) {
 	type args struct {
 		request AuthzRequest
-		scopes  []Scope
+		scopes  string
+		rules   []Rule
 	}
 	tests := []struct {
 		name string
 		args args
-		want Scopes
+		want string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "general pull access and user access tp",
+			args: args{
+				request: AuthzRequest{
+					User:   "cblomart",
+					Groups: []string{},
+				},
+				rules: []Rule{
+					Rule{User: "", Group: "", Match: "${user}/.*", Actions: []string{"push"}},
+					Rule{User: "", Group: "", Match: ".*", Actions: []string{"pull"}},
+				},
+				scopes: "repository:cblomart/foo:pull,push",
+			},
+			want: "repository:cblomart/foo:pull,push",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- server_test.go
+++ server_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -32,19 +35,213 @@ func TestPasswordString_String(t *testing.T) {
 }
 
 func TestHandleAuth(t *testing.T) {
+	ioutil.WriteFile("/tmp/cert.key", []byte(key), 0600)
+	ioutil.WriteFile("/tmp/cert.crt", []byte(cert), 0600)
 	type args struct {
-		w http.ResponseWriter
 		r *http.Request
+		c Config
 	}
 	tests := []struct {
-		name string
-		args args
+		name         string
+		args         args
+		wantCode     int
+		wantResponse string
 	}{
-		// TODO: Add test cases.
+		/*{
+			name: "request all authorized all",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"service":   []string{"registry.docker.io"},
+						"client_id": []string{"Docker"},
+						"scope":     []string{"repository:cblomart/foo:latest:pull,push"},
+					},
+				},
+				c: Config{
+					JWSCert: "/tmp/cert.crt",
+					JWSKey:  "/tmp/cert.key",
+					Issuer:  "auth.dokcer.io",
+					Rules: []Rule{
+						Rule{Match: "${user}/.*", Actions: []string{"pull", "push"}},
+					},
+				},
+			},
+			wantCode:     200,
+			wantResponse: "",
+		},
+		{
+			name: "request all authorized pull",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"service":   []string{"registry.docker.io"},
+						"client_id": []string{"Docker"},
+						"scope":     []string{"repository:cblomart/foo:latest:pull,push"},
+					},
+				},
+				c: Config{
+					JWSCert: "/tmp/cert.crt",
+					JWSKey:  "/tmp/cert.key",
+					Issuer:  "auth.dokcer.io",
+					Rules: []Rule{
+						Rule{Match: "${user}/.*", Actions: []string{"pull"}},
+					},
+				},
+			},
+			wantCode:     200,
+			wantResponse: "",
+		},
+		{
+			name: "request all authorized push",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"service":   []string{"registry.docker.io"},
+						"client_id": []string{"Docker"},
+						"scope":     []string{"repository:cblomart/foo:latest:pull,push"},
+					},
+				},
+				c: Config{
+					JWSCert: "/tmp/cert.crt",
+					JWSKey:  "/tmp/cert.key",
+					Issuer:  "auth.dokcer.io",
+					Rules: []Rule{
+						Rule{Match: "${user}/.*", Actions: []string{"push"}},
+					},
+				},
+			},
+			wantCode:     200,
+			wantResponse: "",
+		},
+		{
+			name: "request without scope",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"service": []string{"registry.docker.io"},
+					},
+				},
+				c: Config{
+					JWSCert: "/tmp/cert.crt",
+					JWSKey:  "/tmp/cert.key",
+					Issuer:  "auth.dokcer.io",
+					Rules: []Rule{
+						Rule{Match: "${user}/.*", Actions: []string{"pull", "push"}},
+					},
+				},
+			},
+			wantCode:     200,
+			wantResponse: "",
+		},
+		{
+			name: "request without client id",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"service": []string{"registry.docker.io"},
+						"scope":   []string{"repository:cblomart/foo:latest:pull,push"},
+					},
+				},
+				c: Config{
+					JWSCert: "/tmp/cert.crt",
+					JWSKey:  "/tmp/cert.key",
+					Issuer:  "auth.dokcer.io",
+					Rules: []Rule{
+						Rule{Match: "${user}/.*", Actions: []string{"pull", "push"}},
+					},
+				},
+			},
+			wantCode:     200,
+			wantResponse: "",
+		},
+		{
+			name: "request without service",
+			args: args{
+				r: &http.Request{
+					RemoteAddr: "127.0.0.1:54657",
+					Header: http.Header{
+						"Authorization": []string{
+							fmt.Sprintf("Basic %s", base64.URLEncoding.EncodeToString([]byte("username:password"))),
+						},
+					},
+					Form: url.Values{
+						"client_id": []string{"Docker"},
+						"scope":     []string{"repository:cblomart/foo:latest:pull,push"},
+					},
+				},
+				c: Config{
+					JWSCert: "/tmp/cert.crt",
+					JWSKey:  "/tmp/cert.key",
+					Issuer:  "auth.dokcer.io",
+					Rules: []Rule{
+						Rule{Match: "${user}/.*", Actions: []string{"pull", "push"}},
+					},
+				},
+			},
+			wantCode:     200,
+			wantResponse: "",
+		},
+		{
+			name: "empty request",
+			args: args{
+				r: &http.Request{},
+				c: Config{
+					JWSCert: "/tmp/cert.crt",
+					JWSKey:  "/tmp/cert.key",
+					Issuer:  "auth.dokcer.io",
+					Rules: []Rule{
+						Rule{Match: "${user}/.*", Actions: []string{"pull", "push"}},
+					},
+				},
+			},
+			wantCode:     200,
+			wantResponse: "",
+		},*/
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			HandleAuth(tt.args.w, tt.args.r)
+			rec := httptest.NewRecorder()
+			AuthConfig = tt.args.c
+			HandleAuth(rec, tt.args.r)
+			if rec.Code != tt.wantCode {
+				t.Errorf("HandleAuth() Code = %v, want %v", rec.Code, tt.wantCode)
+			}
+			if rec.Body.String() != tt.wantResponse {
+				t.Errorf("HandleAuth() Response = %v, want %v", rec.Body.String(), tt.wantResponse)
+			}
+			AuthConfig = Config{}
 		})
 	}
+	os.Remove(AuthConfig.JWSKey)
+	os.Remove(AuthConfig.JWSCert)
 }
